75c18e2fd54e6ad3a1dd87d8ea8104d4f3eee3e3
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-05 11:16:20 -0300

    configuramos un puerto para el repo

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import express from 'express';
+import express from "express";
 
-console.log('bienvenidos');
+console.log("bienvenidos");
 
 // 1 - configurar un puerto
+const app = express();
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
 
 // 2 - configurar middlewares
 
