a9330e188d30f63f095af92d6691b3271d6596a2
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-05 11:35:24 -0300

    realizamos la configuracion del index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config"; //permite procesar variables de entorno
 
 console.log("bienvenidos");
 
@@ -11,5 +14,13 @@ app.listen(app.get("port"), () => {
 });
 
 // 2 - configurar middlewares
+app.use(cors()); //me permite aceptar conexiones remotas
+app.use(morgan("dev")); //para ver mensajes en consola
+app.use(express.json()); //permite interpretar el formato json
+app.use(express.urlencoded({ extended: true })); //permite interpretar los datos del body de una solicitud
 
 // 3 - configuracion de las rutas
+app.get("/", (req, res) => {
+  console.log("hola mundo");
+  res.send("desde el backend de rollingCoffee");
+});
