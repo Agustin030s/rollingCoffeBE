7d3cdbc69e2eaa8a8dabd72ff6e1d5fffb183e98
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-10 16:39:37 -0300

    agregamos el archivo index.html como archivo estatico

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config"; //permite procesar variables de entorno
+import path from "path";
+import { fileURLToPath } from "url";
 
 console.log("bienvenidos");
 
@@ -15,9 +17,14 @@ app.listen(app.get("port"), () => {
 
 // 2 - configurar middlewares
 app.use(cors()); //me permite aceptar conexiones remotas
-app.use(morgan("dev")); //para ver mensajes en consola
+app.use(morgan("dev")); //muestra información extra en la consola
 app.use(express.json()); //permite interpretar el formato json
 app.use(express.urlencoded({ extended: true })); //permite interpretar los datos del body de una solicitud
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 // 3 - configuracion de las rutas
 app.get("/", (req, res) => {
