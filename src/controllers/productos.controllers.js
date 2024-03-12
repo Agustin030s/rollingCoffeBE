152aaf19f4eb24a2e13e55122a7da7a070b59dd7
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-12 11:27:06 -0300

    agregamos el controlador crear producto con la logica necesaria para crear un nuevo producto en la base de datos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,4 +1,26 @@
+import Producto from "../database/models/producto.js";
+
 export const listarProductos = (req, res) => {
-    console.log('hola comision');
-    res.send('aqui listar los productos');
-}
+  console.log("hola comision");
+  res.send("aqui listar los productos");
+};
+
+export const crearProducto = async (req, res) => {
+  try {
+    //extraer los datos del body
+    console.log(req.body);
+    //agregar validacion de los datos del body
+    const productoNuevo = new Producto(req.body);
+    //pedirle a la base de datos que guarde el producto nuevo
+    await productoNuevo.save();
+    //enviar la respuesta al front
+    res.status(201).json({
+      message: "El producto fue creado exitosamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "El producto no pudo ser dado de alta",
+    });
+  }
+};
